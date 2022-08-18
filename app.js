b4930c7dcd70190ef28d4a8910bf62e2cf57b1d7
nigelmarongere <nigelmarongere@gmail.com>
2022-08-18 17:19:31 +0100

    day two workshop completed

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ app.get('/', (req, res) => {
     res.sendStatus(200)
 });
 
+// WEEK 7 DAY 1
 app.get('/feta', async (req, res) => {
     // Query database for data of Feta cheese 
     const givenCheese = await Cheese.findOne({ 
@@ -45,6 +46,47 @@ app.get('/starts-with-f', async (req, res) => {
         };
     });
     res.json(startsWithF);
+});
+
+// WEEK 7 DAY 2
+app.get('/cheeses/:type', async (req, res) => {
+    const find = req.params.type
+    const result = await Cheese.findOne({
+        where: {
+            title: find
+        }
+    })
+    if(result === null){
+        res.sendStatus(404)
+    }else{
+        res.json(result.description)
+    }
+});
+
+// localhost:3000/starts-with?letter=C; req.query returns e.g { letter: 'C'}
+app.get('/starts-with', async (req, res) => {
+    const letter = req.query.letter;
+    const cheeseArray = await Cheese.findAll();
+    const startsWithLetter = new Array();
+    const filterSelection = cheeseArray.filter((cheese) => {
+        if(cheese.title[0] === letter){
+            startsWithLetter.push(cheese.title)
+        };
+    });
+    res.json(startsWithLetter);
+});
+
+// Getting data using both route and query params
+// localhost:3000/cheeses/Feta?provide=description; req.query returns e.g. { provide: 'description' }
+app.get('/cheeses/:stated', async (res, req) => {
+    const statedCheese = req.params.stated;
+    const result = await Cheese.findOne({
+        where: {
+            title: statedCheese
+        }
+    });
+    const { provide } = req.query;
+    res.json(result[provide]);
 });
 
 app.listen(port, () => {
